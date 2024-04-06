3acc0d22c38b6da59d0c3654bc4385bd6e92dd47
CohenNoa <nono9600@gmail.com>
2024-04-06 19:04:53 +0300

    login flow is working

--- app/src/main/java/com/example/finalproject/activities/LoginActivity.kt
+++ app/src/main/java/com/example/finalproject/activities/LoginActivity.kt
@@ -6,6 +6,8 @@ import android.text.TextUtils
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.finalproject.databinding.ActivityLoginBinding
 import com.example.finalproject.repositories.UserRepository
 import com.example.finalproject.viewModels.LoginViewModel
@@ -17,9 +19,32 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding!!.getRoot())
         onNotSingedIn()
+
+        viewModel.loginResult.observe(this, Observer { loggedIn ->
+            if (loggedIn) {
+                binding!!.loginProgressBar.visibility = View.GONE
+
+                // User is already logged in, navigate to another activity
+                changeActivity(MainActivity::class.java)
+
+                // Finish this activity to prevent the user from navigating back to the login screen
+                finish()
+            }
+        })
+
+        viewModel.authenticationFailed.observe(this, Observer
+        {
+            if (it) {
+                // Show toast message for authentication failure
+                showToast("can not log you in.")
+                binding!!.loginProgressBar.visibility = View.GONE
+            }
+        })
+
         onLogin()
         binding!!.loginProgressBar.visibility = View.GONE
     }
@@ -43,26 +68,22 @@ class LoginActivity : AppCompatActivity() {
 
     private fun onLogin() {
         binding!!.loginBtn.setOnClickListener {
-            binding!!.loginProgressBar.visibility = android.view.View.VISIBLE
             val mail: String = binding!!.mailTp.text.toString()
             val password: String = binding!!.passwordTp.text.toString()
+
             if (TextUtils.isEmpty(mail)) {
-                Toast.makeText(this@LoginActivity, "enter mail", Toast.LENGTH_SHORT)
-                    .show()
+                showToast("enter mail")
             } else if (TextUtils.isEmpty(password)) {
-                Toast.makeText(this@LoginActivity, "enter password", Toast.LENGTH_SHORT)
-                    .show()
+                showToast("enter password")
             } else {
-                if (viewModel.login(mail, password)) {
-                    changeActivity(MainActivity::class.java)
-                } else {
-                    Toast.makeText(
-                        this@LoginActivity, "can not log you in.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+                binding!!.loginProgressBar.visibility = View.VISIBLE
+                viewModel.login(mail,password)
             }
         }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     private fun changeActivity(activityClass: Class<*>) {
